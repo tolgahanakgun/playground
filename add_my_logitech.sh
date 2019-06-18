c4c218af68ed09ba1681321850aa71264f09b3ad
Tolgahan Akgun <akguntolgahan@gmail.com>
2019-06-18 11:47:11 +0300

    Create add_my_logitech.sh

---
+++ add_my_logitech.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# the script below adds special mouse buttons on Logitech M546
+# mouse wheel left click -> go backward, one page backward on browser
+# mouse wheel right click -> go forward, one page foreward on browser
+# back button near thumb -> volume down
+# front button near thumb -> volume up
+
+sudo apt install xdotool xbindkeys xautomation
+
+xbindkeys --defaults > ~/.xbindkeysrc
+
+cat <<EOT >> ~/.xbindkeysrc
+"xte 'keydown Alt_L' 'keydown Left' 'keyup Left' 'keyup Alt_L'"
+    b:6
+"xte 'keydown Alt_L' 'keydown Right' 'keyup Right' 'keyup Alt_L'"
+    b:7
+"xdotool key XF86AudioLowerVolume"
+    b:8
+"xdotool key XF86AudioRaiseVolume"
+    b:9
+EOT
+
+xbindkeys -f ~/.xbindkeysrc
